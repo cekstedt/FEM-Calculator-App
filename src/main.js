858251000e5f7c71aa3da4fa34408731b848a2ec
Chris Ekstedt <9835700+cekstedt@users.noreply.github.com>
2022-07-15 16:28:59 -0700

    Hook Model (calculator) to View (index.html) through Controller (main.js).

--- src/main.js
+++ src/main.js
@@ -1,3 +1,32 @@
 import Calculator from "./calculator.js";
 
-let calc = new Calculator();
+const calc = new Calculator();
+
+const keys = document.querySelector(".calculator-keys");
+const result = document.querySelector("#result");
+
+const keyMap = {
+  ADD: "+",
+  SUBTRACT: "-",
+  MULTIPLY: "*",
+  DIVIDE: "/",
+  DECIMAL: ".",
+  RESET: "C",
+  DELETE: "D",
+  EQUALS: "=",
+  ...[..."0123456789"].reduce((obj, x) => {
+    obj[x] = x;
+    return obj;
+  }, {})
+};
+
+keys.addEventListener("click", e => {
+  if (e.target.matches("button")) {
+    const key = e.target;
+    const name = key.name;
+    if (Object.keys(keyMap).includes(name)) {
+      calc.press(name);
+      result.textContent = calc.getDisplay();
+    }
+  }
+});
