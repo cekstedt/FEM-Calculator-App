7d2f8a107f59a4e5264616cfc1694d4f811f7122
Chris Ekstedt <9835700+cekstedt@users.noreply.github.com>
2022-07-18 16:28:11 -0700

    Continue framing and styling html/css.

---
+++ src/themeSwitcher.js
@@ -0,0 +1,44 @@
+let themes = {
+  theme_1: {
+    main_background: "hsl(222, 26%, 31%)",
+    toggle_background: "hsl(223, 31%, 20%)",
+    screen_background: "hsl(224, 36%, 15%)",
+    key_background_1: "hsl(225, 21%, 49%)",
+    key_shadow_1: "hsl(224, 28%, 35%)",
+    key_background_2: "hsl(6, 63%, 50%)",
+    key_shadow_2: "hsl(6, 70%, 34%)",
+    key_background_3: "hsl(30, 25%, 89%)",
+    key_shadow_3: "hsl(28, 16%, 65%)",
+    text: "hsl(221, 14%, 31%)",
+    text_equals: "hsl(0, 0, 100%)",
+    text_white: "hsl(0, 0, 100%)"
+  },
+  theme_2: {
+    main_background: "hsl(0, 0%, 90%)",
+    toggle_background: "hsl(0, 5%, 81%)",
+    screen_background: "hsl(0, 0%, 93%)",
+    key_background_1: "hsl(185, 42%, 37%)",
+    key_shadow_1: "hsl(185, 58%, 25%)",
+    key_background_2: "hsl(25, 98%, 40%)",
+    key_shadow_2: "hsl(25, 99%, 27%)",
+    key_background_3: "hsl(45, 7%, 89%)",
+    key_shadow_3: "hsl(35, 11%, 61%)",
+    text: "hsl(60, 10%, 19%)",
+    text_equals: "hsl(0, 0, 100%)",
+    text_white: "hsl(0, 0, 100%)"
+  },
+  theme_3: {
+    main_background: "hsl(268, 75%, 9%)",
+    toggle_background: "hsl(268, 71%, 12%)",
+    screen_background: "hsl(268, 71%, 12%)",
+    key_background_1: "hsl(281, 89%, 26%)",
+    key_shadow_1: "hsl(285, 91%, 52%)",
+    key_background_2: "hsl(176, 100%, 44%)",
+    key_shadow_2: "hsl(177, 92%, 70%)",
+    key_background_3: "hsl(268, 47%, 21%)",
+    key_shadow_3: "hsl(290, 70%, 36%)",
+    text: "hsl(52, 100%, 62%)",
+    text_equals: "hsl(198, 20%, 13%)",
+    text_white: "hsl(0, 0, 100%)"
+  }
+};
