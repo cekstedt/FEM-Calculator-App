d213ce1fc5f87dbec109a55b8e78cf711ea8b110
Chris Ekstedt <9835700+cekstedt@users.noreply.github.com>
2022-07-19 16:20:33 -0700

    Hook up slider to alternate styles, finalize styles.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import Calculator from "./calculator.js";
+import themes from "./themes.js";
 
 const calc = new Calculator();
-
-const keys = document.querySelector(".calculator-keys");
-const result = document.querySelector("#result");
-
 const keyMap = {
   ADD: "+",
   SUBTRACT: "-",
@@ -14,12 +11,16 @@ const keyMap = {
   RESET: "C",
   DELETE: "D",
   EQUALS: "=",
+  //The rest of the key/value pairs are symmetrical.
   ...[..."0123456789"].reduce((obj, x) => {
     obj[x] = x;
     return obj;
   }, {})
 };
 
+// Hook up buttons and result field with Calculator object.
+const keys = document.querySelector(".calculator-keys-container");
+const result = document.getElementById("result");
 keys.addEventListener("click", e => {
   if (e.target.matches("button")) {
     const key = e.target;
@@ -28,5 +29,15 @@ keys.addEventListener("click", e => {
       calc.press(keyMap[name]);
       result.textContent = calc.getDisplay();
     }
+  }
+});
+
+// Hook up range input with theme switcher.
+const range = document.getElementById("theme-selector");
+range.addEventListener("input", e => {
+  const root = document.documentElement;
+  for (const [key, value] of Object.entries(themes[e.target.value])) {
+    console.log(`${key}: ${value};`);
+    root.style.setProperty("--" + key, value);
   }
 });
